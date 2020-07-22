481ddda5af0dacfe04c10ee91f988659dab6754b
Ifeanyi <doc3coder@gmail.com>
2020-07-22 01:10:22 +0100

    clean up code

--- src/components/pages/login/login.js
+++ src/components/pages/login/login.js
@@ -30,80 +30,85 @@ const Login = () => {
   return (
     <>
       <Navbar />
-      <div class="main-content d-sm-flex">
-        <form class="login-box p-md-5 p-2" onSubmit={onSubmit} method="POST">
-          <h2 class="p-sm-3 p-1 welcome-text">
+      <div className="main-content d-sm-flex">
+        <form
+          className="login-box p-md-5 p-2"
+          onSubmit={onSubmit}
+          method="POST"
+        >
+          <h2 className="p-sm-3 p-1 welcome-text">
             Welcome to <br />
             <strong>Fund my Laptop</strong>
           </h2>
 
-          <p class="p-1 p-md-3 login-text mt-md-n4">
+          <p className="p-1 p-md-3 login-text mt-md-n4">
             Help Achieve Your dreams with funding for your laptops at little to
             no cost.
           </p>
           <div>
             <a
               href="#"
-              class="text-center py-3 btnGoogle d-flex justify-content-center align-items-center"
+              className="text-center py-3 btnGoogle d-flex justify-content-center align-items-center"
             >
-              <img class="pr-3" src={googleImg} alt="" />
+              <img className="pr-3" src={googleImg} alt="" />
               Login with Google
             </a>
           </div>
 
-          <div class="my-4 text-center or d-flex align-items-center or-box">
+          <div className="my-4 text-center or d-flex align-items-center or-box">
             <hr />
-            <span class="or-text">or</span>
+            <span className="or-text">or</span>
             <hr />
           </div>
 
-          <div class="form-group">
+          <div className="form-group">
             <input
               type="email"
               placeholder="Email"
               name="email"
-              class="form-control"
+              className="form-control"
               id="email"
               onChange={onChange}
               value={email}
             />
             <p
               id="errorEmail"
-              class="error text-danger text-center text-sm-left"
+              className="error text-danger text-center text-sm-left"
             ></p>
           </div>
-          <div class="form-group">
+          <div className="form-group">
             <input
               type="password"
               placeholder="Password"
               name="password"
-              class="form-control"
+              className="form-control"
               id="password"
               onChange={onChange}
               value={password}
             />
             <p
               id="errorPassword"
-              class="error text-danger text-center text-sm-left"
+              className="error text-danger text-center text-sm-left"
             ></p>
           </div>
           <div>
             <input
               type="submit"
-              class="form-control login-btn btn-fml-secondary"
+              className="form-control login-btn btn-fml-secondary"
               value="Log in"
             />
           </div>
-          <p class="account-info-text text-center py-4">
+          <p className="account-info-text text-center py-4">
             Don't have an account?
-            <a href="signup.html" class="sign-up-link">
+            <a href="signup.html" className="sign-up-link">
+              {' '}
               Sign up
             </a>
           </p>
         </form>
 
-        <div class="login-img-box d-none d-md-block">
-          <img src={loginBg} class="login-img" alt="login FundMyLaptop" />
+        <div className="login-img-box d-none d-md-block">
+          <img src={loginBg} className="login-img" alt="login FundMyLaptop" />
         </div>
       </div>
       <Footer />
--- src/components/pages/signup/signup.js
+++ src/components/pages/signup/signup.js
@@ -1,69 +1,99 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import '../../assets/bootstrap.css';
-import signupCss from './signup.css';
+import './signup.css';
 import googleImg from '../../assets/images/googleicon.svg';
 import signupBg from '../../assets/images/signup-bg.png';
 import Navbar from '../../Navbar';
 import Footer from '../../Footer';
 
+const initialState = {
+  email: '',
+  password: '',
+};
+function reducer(state, { field, value }) {
+  return {
+    ...state,
+    [field]: value,
+  };
+}
 const Signup = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const onChange = (event) => {
+    dispatch({ field: event.target.name, value: event.target.value });
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  const { email, password } = state;
   return (
     <>
       <Navbar />
-      <main class="main-content d-sm-flex">
-        <form class="login-box p-md-5 p-2">
-          <h2 class="p-sm-3 p-1 welcome-text">
+      <main className="main-content d-sm-flex">
+        <form
+          className="login-box p-md-5 p-2"
+          onSubmit={onSubmit}
+          method="POST"
+        >
+          <h2 className="p-sm-3 p-1 welcome-text">
             Welcome to <br />
             <strong>Fund my Laptop</strong>
           </h2>
 
-          <p class="p-1 p-md-3 login-text mt-md-n4">
+          <p className="p-1 p-md-3 login-text mt-md-n4">
             Help Achieve Your dreams with funding for your laptops at little to
             no cost.
           </p>
 
-          <div class="form-group">
+          <div className="form-group">
             <input
               type="email"
               placeholder="Email"
               name="email"
-              class="form-control"
+              className="form-control"
               id="email"
+              onChange={onChange}
+              value={email}
             />
             <p
               id="errorEmail"
-              class="error text-danger text-center text-sm-left"
+              className="error text-danger text-center text-sm-left"
             ></p>
           </div>
-          <div class="form-group">
+          <div className="form-group">
             <input
               type="password"
               placeholder="Password"
               name="password"
-              class="form-control"
+              className="form-control"
               id="password"
+              onChange={onChange}
+              value={password}
             />
             <p
               id="errorPassword"
-              class="error text-danger text-center text-sm-left"
+              className="error text-danger text-center text-sm-left"
             ></p>
           </div>
-          <div class="form-group">
-            <div class="form-check form-control check-area">
+          <div className="form-group">
+            <div className="form-check form-control check-area">
               <input
-                class="form-check-input check"
+                className="form-check-input check"
                 type="checkbox"
                 value=""
                 id="invalidCheck"
                 required
               />
-              <label class="form-check-label ml-1 p-auto" for="invalidCheck">
+              <label
+                className="form-check-label ml-1 p-auto"
+                for="invalidCheck"
+              >
                 I agree to the{' '}
                 <a href="" style={{ color: 'blue' }}>
                   Terms Policy Conditions
                 </a>
               </label>
-              <div class="invalid-feedback">
+              <div className="invalid-feedback">
                 You must agree before submitting.
               </div>
             </div>
@@ -71,35 +101,38 @@ const Signup = () => {
           <div>
             <input
               type="submit"
-              class="form-control login-btn btn-fml-secondary"
+              className="form-control login-btn btn-fml-secondary"
               value="Sign Up"
             />
           </div>
-          <div class="my-4 text-center or d-flex align-items-center or-box">
+          <div className="my-4 text-center or d-flex align-items-center or-box">
             <hr />
-            <span class="or-text">OR</span>
+            <span className="or-text">OR</span>
             <hr />
           </div>
           <div>
-            <a href="#" class="form-control login-btn reg-btn btn-fml-outline">
-              <img class="pr-3" src={googleImg} alt="" />
+            <a
+              href="#"
+              className="form-control login-btn reg-btn btn-fml-outline"
+            >
+              <img className="pr-3" src={googleImg} alt="" />
               Login with Google
             </a>
           </div>
 
-          <p class="account-info-text text-center py-4 mb-md-3">
+          <p className="account-info-text text-center py-4 mb-md-3">
             Already have an account?{' '}
             <a href="login.html" style={{ color: '#fb3f5c' }}>
               Sign In
             </a>
           </p>
 
-          {/* <div id="error" class="error p-1 "></div>
+          {/* <div id="error" className="error p-1 "></div>
             </div> */}
         </form>
 
-        <div class="login-img-box d-none d-md-block">
-          <img src={signupBg} class="login-img" alt="login FundMyLaptop" />
+        <div className="login-img-box d-none d-md-block">
+          <img src={signupBg} className="login-img" alt="login FundMyLaptop" />
         </div>
       </main>
       <Footer />
