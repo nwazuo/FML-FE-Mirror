758e5f13c5295ba1d19b1e146debae1f1b1e3e7f
qobitech <qobitek@gmail.com>
2020-07-22 20:07:56 +0100

    .

--- src/components/pages/sample/Sample.js
+++ src/components/pages/sample/Sample.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import SampleCss from './Sample.module.css'
+import { Navbar,Footer } from '../navigation/navigation';
 
 const Sample = () => {
     return(
+        <>
+        <Navbar/>
         <div className={SampleCss.sample}>
          
             <h1>Sample Page</h1>
           
         </div>
+        <Footer/>
+        </>
     )
 }
 
