41bc90954ae4a0996baf3b456799aa3f8e064e04
Umoren <samuelumoren365@gmail.com>
2020-07-23 09:19:30 +0100

    Updated Navbar

--- src/components/pages/navigation/Navbar/Navbar.js
+++ src/components/pages/navigation/Navbar/Navbar.js
@@ -24,35 +24,32 @@ const Navbar = (props) => {
         
             <div className="collapse navbar-collapse text-center" id="exCollapsingNavbar">
             <ul className="navbar-nav ml-auto">
-                <li className="nav-item dropdown">
-                    <Link className="nav-link dropdown-toggle" to="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Fundraise for
-                    </Link>
-                    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <Link className="dropdown-item" to="#">Action</Link>
-                        <Link className="dropdown-item" to="#">Another action</Link>
-                        <div className="dropdown-divider"></div>
-                        <Link className="dropdown-item" to="#">Something else here</Link>
-                    </div>
-                </li>
+                
               
                 <li className="nav-item ">
                     <Link className="nav-link" 
                         to="#"
-                        property>Invest <span className="sr-only">(current)</span></Link>
+                        property>FUND <span className="sr-only">(current)</span></Link>
                 </li>
 
                 <li className="nav-item ">
-                    <Link className="nav-link" to={pageurl.ABOUT_US_PAGE_URL}>About Us <span className="sr-only">(current)</span></Link>
+                    <Link className="nav-link" 
+                        to="#"
+                        property>REQUEST <span className="sr-only">(current)</span></Link>
+                </li>
+
+
+                <li className="nav-item ">
+                    <Link className="nav-link" to={pageurl.ABOUT_US_PAGE_URL}>ABOUT <span className="sr-only">(current)</span></Link>
                 </li>
 
                 <li className="nav-item ">
-                    <Link className="btn btn-outline-fml-secondary" to={pageurl.LOGIN_PAGE_URL}>Sign in <span className="sr-only">(current)</span></Link>
+                    <Link className="btn btn-fml-secondary" to={pageurl.LOGIN_PAGE_URL}>LOGIN <span className="sr-only">(current)</span></Link>
                 </li>
 
-                <li className="nav-item ">
+                {/* <li className="nav-item ">
                     <button className="btn btn-fml-secondary" to="#">Start a campaign <span className="sr-only">(current)</span></button>
-                </li>
+                </li> */}
              
             </ul>
          
