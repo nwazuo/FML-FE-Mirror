70a98fd60d5afdc1800cb31d7ee32a33ae018fc0
qobitech <qobitek@gmail.com>
2020-07-23 17:17:37 +0100

    .

--- src/App.js
+++ src/App.js
@@ -3,61 +3,53 @@ import './App.css';
 import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 // import ProtectedRoute from './components/router/protectedroute/ProtectedRoute'
 import pageurl from './components/router/url/pageurl'
-import {Login,SignUp,Terms,PrivacyPage,FAQ,ContactUs,UserProfile,Recommendations,
-  LandingPage,AboutUs,Page404,ComingSoon} from './components/pages/mainpages'
+import {Login,SignUp,Terms,PrivacyPage,FAQ,ContactUs,
+        UserProfile,Recommendations,LandingPage,AboutUs,
+        Page404,ComingSoon} from './components/pages/mainpages'
 
 function App() {
-	return (
-		<Router basename={process.env.PUBLIC_URL}>
-			<div className="maincon">
-				{/* <Navbar/> */}
 
-				<Switch>
-					<ScrollIntoView>
-						<Route
-							exact
-							path={pageurl.COMING_SOON_URL}
-							component={ComingSoon}
-						/>
+  return (
+        
+    <Router basename={process.env.PUBLIC_URL}>
 
-						<Route
-							exact
-							path={pageurl.LANDING_PAGE_URL}
-							component={LandingPage}
-						/>
+          <Switch>
 
-						<Route exact path={pageurl.LOGIN_PAGE_URL} component={Login} />
+                <Route exact path={pageurl.LANDING_PAGE_URL} component={LandingPage} />
 
-						<Route exact path={pageurl.REGISTER_PAGE_URL} component={SignUp} />
+                <Route exact path={pageurl.COMING_SOON_URL} component={LandingPage} />
+                
+                <Route exact path={pageurl.LOGIN_PAGE_URL} component={Login} />
 
-						<Route exact path={pageurl.TC_PAGE_URL} component={Terms} />
+                <Route exact path={pageurl.REGISTER_PAGE_URL} component={SignUp} />
 
-						<Route
-							exact
-							path={pageurl.PRIVACY_POLICY_PAGE_URL}
-							component={PrivacyPage}
-						/>
+                <Route exact path={pageurl.TC_PAGE_URL} component={Terms} />
 
-						<Route exact path={pageurl.FAQ_PAGE_URL} component={FAQ} />
+                <Route exact path={pageurl.ABOUT_US_PAGE_URL} component={AboutUs} />
 
-						<Route
-							exact
-							path={pageurl.CONTACT_US_PAGE_URL}
-							component={ContactUs}
-						/>
-					</ScrollIntoView>
+                {/* <ProtectedRoute exact path={pageurl.LOGINURL} component={Login} history={history} /> */}
 
-					{/* <ProtectedRoute exact path={pageurl.LOGINURL} component={Login} history={history} /> */}
+                <Route exact path={pageurl.USER_PROFILE_PAGE_URL} component={UserProfile} />
 
-					{/* <Route path="*" component={Page404}/> */}
+                <Route exact path={pageurl.USER_RECOMMENDATIONS} component={Recommendations} />
 
-					{/* <Route component={Page404}/> */}
-				</Switch>
+                <Route exact path={pageurl.PRIVACY_POLICY_PAGE_URL} component={PrivacyPage} />
 
-				{/* <Footer/> */}
-			</div>
-		</Router>
-	);
+                <Route exact path={pageurl.FAQ_PAGE_URL} component={FAQ} />
+
+                <Route exact path={pageurl.CONTACT_US_PAGE_URL} component={ContactUs} />
+
+                {/* <ProtectedRoute exact path={pageurl.LANDING_PAGE_URL} component={Login}/> */}
+
+                {/* <Route exact path={pageurl.PAGE_404} component={Page404}/> */}
+
+                <Route component={Page404}/>  
+
+          </Switch>
+
+   </Router>
+
+  );
 }
 
 export default App;
