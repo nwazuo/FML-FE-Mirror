eb0c0df66fbc8eede5c8dac22d3554f5728640cb
Adiomojeed <adio.mojeed@gmail.com>
2020-07-24 14:57:11 +0100

    Fix: Added react spinners to enable loading for search results

--- src/components/pages/mainpages/fqa/FQA.js
+++ src/components/pages/mainpages/fqa/FQA.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import BeatLoader from "react-spinners/BeatLoader";
+import { css } from "@emotion/core";
 import FQACss from './FQA.module.css'
 import search from './search.svg'
 import { Navbar,Footer } from '../../navigation/navigation';
@@ -10,7 +12,7 @@ class FQA extends React.Component {
     constructor (props) {
         super (props);
         this.state = {
-            faqs: [],
+            faqs: null,
             value: ""
         }
 
@@ -19,6 +21,7 @@ class FQA extends React.Component {
     }
 
     componentDidMount () {
+        document.title = "FAQ"
         fetch('https://api.fundmylaptop.com/api/faqs')
             .then(res => res.json())
             .then(data => this.setState({faqs: data.data}))
@@ -31,15 +34,27 @@ class FQA extends React.Component {
 
     onHandleSubmit(e) {
         const { value } = this.state
+        this.setState({ faqs: null })
         fetch(`https://api.fundmylaptop.com/api/search/faqs?q=${value}`)
             .then(res => res.json())
             .then(data => this.setState({faqs: data.data}))
             .catch(err => console.error(err))
+        this.setState({ value: ""})
         e.preventDefault()
     }
 
     render () {
         const { value, faqs } = this.state
+
+        const override = css`
+			display: block;
+			margin: 40px auto;
+			text-align: center;
+			border-color: #55efc4;
+			color: #04172A;
+			max-width: 100%;
+        `;
+        
         return (
             <ScrollIntoView>
                 <Navbar/>
@@ -77,22 +92,26 @@ class FQA extends React.Component {
                                 <div className="col-12 col-md-9">
                                     <h2 className="mb-4 mt-4 mt-lg-3">Getting Started</h2>
                                     <div id="accordion">
-                                        {faqs.map((data, idx) => (
-                                            <div className="card mb-4" key={data._id}>
-                                                <div className={[FQACss.accordion_header, "card-header"].join(' ')} id={`heading${idx}`}>
-                                                    <h5 className="mb-0">
-                                                        <button className={["pt-md-2", FQACss.no_border].join(' ')} data-toggle="collapse" data-target={`#collapse${idx}`} aria-expanded="true" aria-controls={`collapse${idx}`}>
-                                                            <h5 className="text-white">{data.question}</h5>
-                                                        </button>
-                                                    </h5>
-                                                </div>
-                                                <div id={`collapse${idx}`} className="collapse show" aria-labelledby={`heading${idx}`} data-parent="#accordion">
-                                                    <div className="card-body">
-                                                        {data.answer}
+                                        {!faqs ? (
+                                            <BeatLoader css={override} size={40} color={"#04172A"} />
+                                            ) : (
+                                            faqs.map((data, idx) => (
+                                                <div className="card mb-4" key={data._id}>
+                                                    <div className={[FQACss.accordion_header, "card-header"].join(' ')} id={`heading${idx}`}>
+                                                        <h5 className="mb-0">
+                                                            <button className={["pt-md-2", FQACss.no_border].join(' ')} data-toggle="collapse" data-target={`#collapse${idx}`} aria-expanded="true" aria-controls={`collapse${idx}`}>
+                                                                <h5 className="text-white">{data.question}</h5>
+                                                            </button>
+                                                        </h5>
+                                                    </div>
+                                                    <div id={`collapse${idx}`} className="collapse show" aria-labelledby={`heading${idx}`} data-parent="#accordion">
+                                                        <div className="card-body">
+                                                            {data.answer}
+                                                        </div>
                                                     </div>
                                                 </div>
-                                            </div>
-                                        ))}
+                                            ))
+                                        )}
                                     </div>
                                 </div>
                             </div>
