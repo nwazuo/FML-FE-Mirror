8f7b0a209d6c96e61683b537710ba60f5b4f20bb
Adiomojeed <adio.mojeed@gmail.com>
2020-07-24 12:11:47 +0100

    Feat: consumed the faq api & Fix: refactored the faq page

--- src/components/pages/mainpages/fqa/FQA.js
+++ src/components/pages/mainpages/fqa/FQA.js
@@ -1,126 +1,92 @@
 import React from 'react';
 import FQACss from './FQA.module.css'
-import seach from './search.svg'
+import search from './search.svg'
 import { Navbar,Footer } from '../../navigation/navigation';
 import ScrollIntoView from '../../../router/scrollintoview/ScrollIntoView'
 
 
-const FQA = () => {
-  
-     
-    function handleClick(e) {
-        e.preventDefault();
-        document.getElementById("info1").classList.toggle(FQACss.none);
-        document.getElementById("info1-s-1").classList.toggle(FQACss.none);
-        document.getElementById("info1-s-2").classList.toggle(FQACss.show);
-      }
+class FQA extends React.Component {
 
-      function handleClick1(e) {
-        e.preventDefault();
-        
-        document.getElementById("info2").classList.toggle(FQACss.show);
-        document.getElementById("info2-s-1").classList.toggle(FQACss.none);
-        document.getElementById("info2-s-2").classList.toggle(FQACss.show);
-      }
+    constructor (props) {
+        super (props);
+        this.state = {
+            faqs: [],
+            value: ""
+        }
 
-      function handleClick2(e) {
-        e.preventDefault();
-        document.getElementById("info3").classList.toggle(FQACss.show);
-        document.getElementById("info3-s-1").classList.toggle(FQACss.none);
-        document.getElementById("info3-s-2").classList.toggle(FQACss.show);
-      }
+        this.onHandleChange = this.onHandleChange.bind(this)
+    }
 
-      function handleClick3(e) {
-        e.preventDefault();
-        document.getElementById("info4").classList.toggle(FQACss.show);
-        document.getElementById("info4-s-1").classList.toggle(FQACss.none);
-        document.getElementById("info4-s-2").classList.toggle(FQACss.show);
-      }
-   
-    return(
-       <ScrollIntoView>
-        <Navbar/>
-        <div className={FQACss.fqa}>
-            <div className={FQACss.container}>
-                <h1 className={FQACss.h1}>How can we help you</h1>
-                <div className={FQACss.grid}>
-                <input className={FQACss.input1} type="text"  placeholder="Describe your issue"></input><button  className={FQACss.btn} >
-                    <img src={seach} alt="search-bar"></img>
-                </button>
-                </div>
-                <p className={FQACss.p}>You can also browse the topics below to find what you’re looking for</p>
-            </div>
-           <div className={FQACss.questions}>
-                <div className={FQACss.container1}>
-                
-                <ul>
-                     <h1 className={FQACss.h2}>Getting Started</h1>
-                    <li className={FQACss.fqaLinks}>Funding</li>
-                    <li className={FQACss.fqaLinks}>Menu III</li>
-                    <li className={FQACss.fqaLinks}>Menu IV</li>
-                    <li className={FQACss.fqaLinks}>Menu V</li>
-                </ul>
-                </div>               
-                <div  >
-                <h1 className={FQACss.h3}>Getting Started</h1>
-                <div className={FQACss.container2} >
-                    <div className={FQACss.headInfo}>
-                        <p className={FQACss.pTop}>What is FundMy<span className={FQACss.red} >Laptop</span> and how does it work ? </p> <p onClick={handleClick}  ><span className={FQACss.minplus} id="info1-s-1">-</span><span className={FQACss.minplus2} id="info1-s-2">+</span></p>
-                    </div>
-                    <div className={FQACss.divP2show} id="info1">
-                        <p className={FQACss.p2} >
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex odit rem amet sed! Minus maiores aperiam, saepe, magnam voluptate at nam repellendus cumque aliquid tempore, excepturi eum nesciunt in a!
-                        </p>
-                    </div>
-                </div>
+    componentDidMount () {
+        fetch('https://api.fundmylaptop.com/api/faqs')
+            .then(res => res.json())
+            .then(data => this.setState({faqs: data.data}))
+            .catch(err => console.err(err))
+    }
 
-                {/* second */}
-                <div className={FQACss.container2} >
-                    <div className={FQACss.headInfo}>
-                        <p  className={FQACss.pTop}>In what countries can i apply for a laptop ? </p> <p onClick={handleClick1} ><span className={FQACss.minplus} id="info2-s-1">+</span><span className={FQACss.minplus2} id="info2-s-2">-</span></p>
-                    </div>
-                    <div id="info2" className={FQACss.divP2} >
-                        <p  className={FQACss.p2} >
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex odit rem amet sed! Minus maiores aperiam, saepe, magnam voluptate at nam repellendus cumque aliquid tempore, excepturi eum nesciunt in a!
-                        </p>
-                    </div>
-                </div>
+    onHandleChange(e) {
+        this.setState({value: e.target.value})
+    }
 
-                {/* thried */}
-
-                <div className={FQACss.container2} >
-                    <div className={FQACss.headInfo}>
-                        <p  className={FQACss.pTop}>Question III ? </p><p onClick={handleClick2} ><span className={FQACss.minplus} id="info3-s-1">+</span><span className={FQACss.minplus2} id="info3-s-2">-</span></p>
-                    </div>
-                    <div className={FQACss.divP2} id="info3">
-                        <p className={FQACss.p2}>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex odit rem amet sed! Minus maiores aperiam, saepe, magnam voluptate at nam repellendus cumque aliquid tempore, excepturi eum nesciunt in a!
-                        </p>
-                    </div>
-                </div>
-
-                {/* forth */}
-
-                <div className={FQACss.container2} >
-                    <div className={FQACss.headInfo}>
-                    <p className={FQACss.pTop}>Question Iv ? </p><p onClick={handleClick3} ><span className={FQACss.minplus} id="info4-s-1">+</span><span className={FQACss.minplus2} id="info4-s-2">-</span></p>
-                    </div>
-                    <div className={FQACss.divP2} id="info4">
-                        <p className={FQACss.p2}>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex odit rem amet sed! Minus maiores aperiam, saepe, magnam voluptate at nam repellendus cumque aliquid tempore, excepturi eum nesciunt in a!
-                        </p>
-                    </div>
-                </div>
-                </div>               
-           </div>
-           <div className={FQACss.pdT}><h1 className={FQACss.h1}> Looking For Something Else ?</h1>
-            <div className={FQACss.center}>
-                <a href="/"  className={FQACss.contactBtn}>Contact us <span className={FQACss.greaterthan}>&gt;</span></a>
-            </div>  </div>        
-        </div>
-       <Footer/>
-       </ScrollIntoView>
-    )
+    render () {
+        return (
+            <ScrollIntoView>
+                <Navbar/>
+                    <section className={[FQACss.custom__py_main, FQACss.bg_light, "container-fluid"].join(' ')}>
+                        <div className="row justify-content-center">
+                            <div className="col-12 col-md-9 col-xl-6">
+                                <h2 className="text-center">How can we help you?</h2>
+                                <div className="position-relative my-4 px-2 px-lg-5">
+                                    <input 
+                                        type="text" 
+                                        className="form-control w-100" 
+                                        value={this.state.value}
+                                        onChange={this.onHandleChange}
+                                    />
+                                    <img src={search} className={[FQACss.search_icon, "img-fluid"].join(' ')} alt="search" />
+                                </div>
+                                <h3 className={[FQACss.text_light, "text-center mb-0 mt-0"].join(' ')}>You can also browse the topics below to find what you’re looking for</h3>
+                            </div>
+                        </div>
+                    </section>
+                    <section className="container my-md-4 px-3 px-md-0">
+                        <div className="row">
+                            <div className="col-12 col-md-3">
+                                <h6>Getting Started</h6>
+                                <div className="d-flex flex-column">
+                                    <h6 className="py-0 my-3"><a className={[FQACss.links].join(' ')} href="/">Funding </a></h6>
+                                    <h6 className="py-0 my-3"><a className={[FQACss.links].join(' ')} href="/">Menu III </a></h6>
+                                    <h6 className="py-0 my-3"><a className={[FQACss.links].join(' ')} href="/">Menu IV </a></h6>
+                                    <h6 className="py-0 my-3"><a className={[FQACss.links].join(' ')} href="/">Menu V </a></h6>
+                                </div>
+                            </div>
+                            <div className="col-12 col-md-9">
+                                <h2 className="mb-4 mt-4 mt-lg-3">Getting Started</h2>
+                                <div id="accordion">
+                                    {this.state.faqs.map((data, idx) => (
+                                        <div className="card mb-4" key={data._id}>
+                                            <div className={[FQACss.accordion_header, "card-header"].join(' ')} id={`heading${idx}`}>
+                                                <h5 className="mb-0">
+                                                    <button className={["pt-md-2", FQACss.no_border].join(' ')} data-toggle="collapse" data-target={`#collapse${idx}`} aria-expanded="true" aria-controls={`collapse${idx}`}>
+                                                        <h5 className="text-white">{data.question}</h5>
+                                                    </button>
+                                                </h5>
+                                            </div>
+                                            <div id={`collapse${idx}`} className="collapse show" aria-labelledby={`heading${idx}`} data-parent="#accordion">
+                                                <div className="card-body">
+                                                    {data.answer}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                <Footer/>
+            </ScrollIntoView>
+        )
+    }
 }
 
 export default FQA;
