f7bd7a0436bf1e61b8616ff5592f63942841b1c4
Nierowheezy <olaniyiolabode99@yahoo.com>
2020-07-24 02:48:46 +0100

    Title: Email Verification Description : Error and success Pages

--- src/components/pages/mainpages/mainpages.js
+++ src/components/pages/mainpages/mainpages.js
@@ -8,21 +8,24 @@ import ContactUs from './contact/Contact';
 import LandingPage from './landingPage/landingPage';
 import UserProfile from './user-profile/user-profile';
 import Recommendations from './recommendations/recommendations';
-import Page404 from './page404/Page404'
+import Page404 from './page404/Page404';
 import ComingSoon from './comingSoon/comingSoon';
-
+import EmailConfirmationSuccess from './EmailConfirmation/EmailConfirmationSuccess/EmailConfirmationSuccess';
+import EmailConfirmationFailure from './EmailConfirmation/EmailConfirmationFailure/EmailConfirmationFailure';
 
 export {
-    Login,
-    SignUp,
-    Terms,
-    PrivacyPage,
-    FAQ,
-    ContactUs,
-    LandingPage,
-    UserProfile,
-    Recommendations,
-    AboutUs,
-    Page404,
-    ComingSoon
-}
+  Login,
+  SignUp,
+  Terms,
+  PrivacyPage,
+  FAQ,
+  ContactUs,
+  LandingPage,
+  UserProfile,
+  Recommendations,
+  AboutUs,
+  Page404,
+  ComingSoon,
+  EmailConfirmationSuccess,
+  EmailConfirmationFailure
+};
--- src/components/router/url/pageurl.js
+++ src/components/router/url/pageurl.js
@@ -1,32 +1,36 @@
 // PUBLIC PAGES
-const LANDING_PAGE_URL = "/";
-const LOGIN_PAGE_URL = "/login";
-const USER_PROFILE_PAGE_URL = "/user-profile";
-const USER_RECOMMENDATIONS = "/recommendations"
-const REGISTER_PAGE_URL = "/signup";
-const ABOUT_US_PAGE_URL = "/about";
-const CONTACT_US_PAGE_URL = "/contact-us"
-const PRIVACY_POLICY_PAGE_URL = "/privacy"
-const FAQ_PAGE_URL = "/FAQ"
-const TC_PAGE_URL = "/terms-and-conditions"
-const PAGE_404 = "/404"
+const LANDING_PAGE_URL = '/';
+const LOGIN_PAGE_URL = '/login';
+const USER_PROFILE_PAGE_URL = '/user-profile';
+const USER_RECOMMENDATIONS = '/recommendations';
+const REGISTER_PAGE_URL = '/signup';
+const ABOUT_US_PAGE_URL = '/about';
+const CONTACT_US_PAGE_URL = '/contact-us';
+const PRIVACY_POLICY_PAGE_URL = '/privacy';
+const FAQ_PAGE_URL = '/FAQ';
+const TC_PAGE_URL = '/terms-and-conditions';
+const PAGE_404 = '/404';
 const COMING_SOON_URL = '/coming-soon';
-const FUNDEE_DASHBOARD_PAGE_URL = "/fundee-dashboard"
+const FUNDEE_DASHBOARD_PAGE_URL = '/fundee-dashboard';
+const EMAIL_CONFIRMATION_SUCCESS_URL = '/email-confirmation-success';
+const EMAIL_CONFIRMATION_FAILURE_URL = '/email-confirmation-failure';
 
 const pageurl = {
-    LANDING_PAGE_URL,
-    LOGIN_PAGE_URL,
-    USER_PROFILE_PAGE_URL,
-    USER_RECOMMENDATIONS,
-    REGISTER_PAGE_URL,
-    ABOUT_US_PAGE_URL,
-    CONTACT_US_PAGE_URL,
-    PRIVACY_POLICY_PAGE_URL,
-    FAQ_PAGE_URL,
-    TC_PAGE_URL,
-    PAGE_404,
-    COMING_SOON_URL,
-    FUNDEE_DASHBOARD_PAGE_URL
-}
+  LANDING_PAGE_URL,
+  LOGIN_PAGE_URL,
+  USER_PROFILE_PAGE_URL,
+  USER_RECOMMENDATIONS,
+  REGISTER_PAGE_URL,
+  ABOUT_US_PAGE_URL,
+  CONTACT_US_PAGE_URL,
+  PRIVACY_POLICY_PAGE_URL,
+  FAQ_PAGE_URL,
+  TC_PAGE_URL,
+  PAGE_404,
+  COMING_SOON_URL,
+  FUNDEE_DASHBOARD_PAGE_URL,
+  EMAIL_CONFIRMATION_SUCCESS_URL,
+  EMAIL_CONFIRMATION_FAILURE_URL
+};
 
 export default pageurl;
