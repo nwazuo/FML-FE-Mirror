e0f24f80d1e6f57caa26b1c0fd65b60d59863b76
Gbolade-True <ibukunoluwa.g.popoola@gmail.com>
2020-07-25 14:01:49 +0100

    changing

--- src/components/dataservices/testimonial_controller.js
+++ src/components/dataservices/testimonial_controller.js
@@ -12,58 +12,38 @@ const Testimonial = () => {
 	});
 
 	return testimonials ? (
-		<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-			<div class="carousel-inner">
-				<div class="carousel-item active">
-					<section
-						className=" container-fluid testimonials-section mt-md-5 row justify-content-center">
-						<h3 className="col-md-8 text-center text-white mb-4">
-							Testimonials</h3>
-						<div className="testimonials-text col-md-9 mb-4">
-							<p className=" text-white px-5">
-								“Graduating university without a job, I needed a laptop computer
-								to kickstart my freelance business. A friend referred my to Fund
-								my Laptop, I had my doubts but they were easily erased, Within
-								an hour of posting a campaign i got full funding for my laptop
-								and my account was immediately credited, Within three months i
+		testimonials.map((testimonial) => {
+			return (
+				<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+					<div class="carousel-inner">
+						<div class="carousel-item active" id={testimonial.id}>
+							<section
+								className=" container-fluid testimonials-section mt-md-5 row justify-content-center">
+								<h3 className="col-md-8 text-center text-white mb-4">
+									Testimonials</h3>
+								<div className="testimonials-text col-md-9 mb-4">
+									<p className=" text-white px-5">
+										“Graduating university without a job, I needed a laptop computer
+										to kickstart my freelance business. A friend referred my to Fund
+										my Laptop, I had my doubts but they were easily erased, Within
+										an hour of posting a campaign i got full funding for my laptop
+										and my account was immediately credited, Within three months i
 								was able to pay back the loan and grow my business”</p>
+								</div>
+								<div className=" col-md-10 text-center ">
+									<img className="mb-3 " src="" alt="testimonial" />
+									<h5 className="mb-0" style={{ color: "white" }}>
+										Abiodun Popoola</h5>
+									<span style={{ color: "white" }} className="card-subtitle">
+										Raise 500k for his macbook pro</span>
+								</div>
+							</section>
 						</div>
-						<div className=" col-md-10 text-center ">
-							<img className="mb-3 " src="" alt="testimonial" />
-							<h5 className="mb-0" style={{ color: "white" }}>
-								Abiodun Popoola</h5>
-							<span style={{ color: "white" }} className="card-subtitle">
-								Raise 500k for his macbook pro</span>
-						</div>
-					</section>
-				</div>
-				{testimonials.map((testimonial) => {
-					<div class="carousel-item">
-						<section
-							className=" container-fluid testimonials-section mt-md-5 row justify-content-center">
-							<h3 className="col-md-8 text-center text-white mb-4">
-								Testimonials</h3>
-							<div className="testimonials-text col-md-9 mb-4">
-								<p className=" text-white px-5">
-									“Graduating university without a job, I needed a laptop computer
-									to kickstart my freelance business. A friend referred my to Fund
-									my Laptop, I had my doubts but they were easily erased, Within
-									an hour of posting a campaign i got full funding for my laptop
-									and my account was immediately credited, Within three months i
-								was able to pay back the loan and grow my business”</p>
-							</div>
-							<div className=" col-md-10 text-center ">
-								<img className="mb-3 " src={testimonial.id} alt="testimonial" />
-								<h5 className="mb-0" style={{ color: "white" }}>
-									Abiodun Popoola</h5>
-								<span style={{ color: "white" }} className="card-subtitle">
-									Raise 500k for his macbook pro</span>
-							</div>
-						</section>
 					</div>
-				})}
-			</div>
-		</div>
+				</div>
+			)
+		})
+
 	) : ''
 
 
