f2b79cdd28e83273c79e4a7be099dcbf777210ed
Nierowheezy <olaniyiolabode99@yahoo.com>
2020-07-27 00:15:54 +0100

    redirect to default dashboard

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {
   SET_USER,
   SET_AUTHENTICATED,
@@ -46,52 +45,3 @@ export default function (state = initialState, action) {
       return state;
   }
 }
-=======
-import {
-  SET_USER,
-  SET_AUTHENTICATED,
-  SET_UNAUTHENTICATED,
-  LOADING_USER,
-  // SET_ERRORS,
-  // CLEAR_ERRORS,
-  REGISTERED_USER,
-} from './types';
-
-const initialState = {
-  authenticated: false,
-  loading: false,
-  credentials: {},
-  errors: null,
-  message: null,
-};
-
-export default function (state = initialState, action) {
-  switch (action.type) {
-    case SET_AUTHENTICATED:
-      return {
-        ...state,
-        authenticated: true,
-      };
-    case SET_UNAUTHENTICATED:
-      return initialState;
-    case SET_USER:
-      return {
-        authenticated: true,
-        loading: false,
-        credentials: action.payload,
-      };
-    case LOADING_USER:
-      return {
-        ...state,
-        loading: false,
-      };
-    case REGISTERED_USER:
-      return {
-        ...state,
-        message: action.payload,
-      };
-    default:
-      return state;
-  }
-}
->>>>>>> 6b771b8ecf0d88d9d0d9757df7d7481e4ef30cdc
