e97ea5c1c05710f8ccd77d27af351558a4cafd2d
Chidi Okpala <63919087+ChidiOkpala@users.noreply.github.com>
2020-07-27 17:20:41 +0100

    investor dashboard update

--- src/components/pages/dashboards/investordashboard/investordashboard.js
+++ src/components/pages/dashboards/investordashboard/investordashboard.js
@@ -273,10 +273,10 @@ class InvestorsDashboard extends Component{
                                     </div>
                                 </div>
     
-                                
                                 <br />
     
                                 <header className='investoraccount-header'>Here is your account overview</header>
+                                
                                 <section className="investoraccount investorpadding">
                                     <div className="investoraccount__block investororder2">
                                         <div className="investoraccount__block--details">
@@ -313,55 +313,77 @@ class InvestorsDashboard extends Component{
                                     <div className="d-flex investororder1">
                                         <div className="col-7 investordontshow">
                                             <h5 className="text-left mb-0 pb-0">Total Invested Amount</h5><br />
-                                            <h6 className="text-left mt-0 pt-0" ><small>INR</small>{investedAmount}</h6>
+                                            <h5 className="text-left mt-0 pt-0" ><small>NGN </small>{investedAmount}</h5>
                                         </div>
                                         <div className="investorspace investordontshow"></div>
-                                        <div className="investoraccount__progress mt-2 mb-3 mr-3">
-                                            <img  src={progress} alt="" />
+                                        <div className="overview__progress-bar mx-auto mt-md-5">
+                                            <svg
+                                            className=""
+                                            width="100%"
+                                            height="100%"
+                                            viewBox="0 0 200 200"
+                                            fill="none"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            >
+                                            <path
+                                                d="M99.9998 8.69548C111.99 8.69548 123.863 11.0571 134.94 15.6456C146.018 20.2341 156.083 26.9595 164.562 35.4379C173.04 43.9163 179.766 53.9816 184.354 65.0592C188.943 76.1367 191.304 88.0096 191.304 99.9998C191.304 111.99 188.943 123.863 184.354 134.941C179.766 146.018 173.04 156.083 164.562 164.562C156.083 173.04 146.018 179.766 134.94 184.354C123.863 188.943 111.99 191.304 99.9998 191.304C88.0095 191.304 76.1367 188.943 65.0591 184.354C53.9816 179.766 43.9163 173.04 35.4379 164.562C26.9595 156.083 20.2341 146.018 15.6456 134.94C11.0571 123.863 8.69548 111.99 8.69548 99.9998C8.69549 88.0095 11.0572 76.1367 15.6456 65.0591C20.2341 53.9816 26.9595 43.9163 35.4379 35.4379C43.9163 26.9595 53.9817 20.2341 65.0592 15.6456C76.1368 11.0571 88.0096 8.69547 99.9999 8.69548L99.9998 8.69548Z"
+                                                stroke="#E1E1E1"
+                                                stroke-width="8.69565"
+                                                stroke-dasharray="1.74 1.74"
+                                            />
+                                            </svg>
+                                            <div className="overview__progress-info d-flex flex-column align-items-center">
+                                                <h5 className="percentage">0%</h5>
+                                                <span>REPAID</span>
+                                            </div>
                                         </div>
                                     </div>
                                     
                                 </section>
     
                                 <section className="investortable investortablepadding">
-                                    <header>Your Investments</header>
+                                   
                                     <div className="investortable__container">
                                         <table>
-                                        <thead className="investorthead">
-                                            <tr>
-                                                <td>LOAN</td>
-                                                <td>AMOUNT</td>
-                                                <td>INTEREST RATE</td>
-                                                <td>TERM</td>
-                                                <td>TOTAL RETURNS</td>
-                                                <td>PAYMENT DUE</td>
-                                                <td>STATUS</td>
-                                            </tr>
-                                            
-                                        </thead>
+                                        {table.length > 0 && !isLoadingTable && (
+                                            <div>
+                                                <header>Your Investments</header>
+                                                <thead className="investorthead">
+                                                    <tr>
+                                                        <td>LOAN</td>
+                                                        <td>AMOUNT</td>
+                                                        <td>INTEREST RATE</td>
+                                                        <td>TERM</td>
+                                                        <td>TOTAL RETURNS</td>
+                                                        <td>PAYMENT DUE</td>
+                                                        <td>STATUS</td>
+                                                    </tr>
+                                                </thead>
+                                            </div>
+                                        )}                                                    
 
-                                        <tbody className="investortbody">
+                                        
                                             {table.length < 1 && isLoadingTable && (
                                                 <BeatLoader css={override} size={40} color={"#04172A"} />
 
                                             )}
-
-                                            {table.length > 0 && !isLoadingTable ? (
+                                            
+                                            
+                                            <tbody className="investortbody">
+                                            {table.length > 0 && !isLoadingTable && (
                                                         
                                                 table.map((row, idx) => (
                                                     <tr>
-                                                        <td>{row.id}</td>
+                                                        <td>{idx}</td>
                                                         <td>{row.amount}</td>
-                                                        <td>{row.rate}</td>
+                                                        <td>{row.interestRate}</td>
                                                         <td>{row.term}</td>
-                                                        <td>{row.returns}</td>
-                                                <td> <span><b className="investorday">{row.paymentdue.day}</b></span> {row.paymentdue.month}</td>
+                                                        <td>{row.totalReturns}</td>
+                                                <td> <span><b className="investorday"></b></span> {row.paymentDue}</td>
                                                         <td>{row.status}</td>
                                                     </tr>
                                                 ))
-                                            ) : (
-                                                <p class="py-3 text-center">There is no investment yet</p>
-                                            )}
+                                            ) }
                                         </tbody>
                                         </table>
                                     </div>
