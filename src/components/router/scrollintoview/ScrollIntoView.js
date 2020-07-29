0ce35ccfb65b745ef659a201ab3fcefa9923bb61
Gbolade-True <ibukunoluwa.g.popoola@gmail.com>
2020-07-29 21:40:00 +0100

    landingPageAPI Updates

--- src/components/router/scrollintoview/ScrollIntoView.js
+++ src/components/router/scrollintoview/ScrollIntoView.js
@@ -3,17 +3,17 @@ import {createBrowserHistory} from 'history';
 
 let oldpath = "";
 const ScrollIntoView =(props)=> {  
-  let history = createBrowserHistory();
-  React.useEffect(()=>{  
-    function doit(){
-      if(history.location.pathname !== oldpath){
-        const body = document.querySelector('#root');
-        body.scrollIntoView({behavior: 'smooth'},0)
-        // eslint-disable-next-line
-        oldpath = history.location.pathname;
-      }
-    }doit()
-  })
+  // let history = createBrowserHistory();
+  // React.useEffect(()=>{  
+  //   function doit(){
+  //     if(history.location.pathname !== oldpath){
+  //       const body = document.querySelector('#root');
+  //       body.scrollIntoView({behavior: 'smooth'},0)
+  //       // eslint-disable-next-line
+  //       oldpath = history.location.pathname;
+  //     }
+  //   }doit()
+  // })
   return(<>{props.children}</>)
 }
 export default ScrollIntoView;
