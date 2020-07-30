e89c356e1bd9c689d04e7d40f81dd3bbf54f737a
Emmanuel Afuadajo <aquilaafuadajo@gmail.com>
2020-07-30 13:36:49 +0100

    added fb-auth to sign-up page

--- src/components/pages/mainpages/signup/signup.js
+++ src/components/pages/mainpages/signup/signup.js
@@ -11,12 +11,14 @@ import { connect } from 'react-redux';
 import {
   registerUser,
   googleLogin,
-  // githubLogin,
+  //githubLogin,
+  facebookLogin,
 } from '../../../../actions/userActions';
 import PinWheel from '../../../ui/loaders/pin-wheel';
 import PinWheelColor from '../../../ui/loaders/pin-wheel-color';
 import Button from '../../../utilities/Button/CustomizedButton';
 import { GoogleLogin } from 'react-google-login';
+import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props';
 class Signup extends Component {
   state = {
     email: '',
@@ -172,6 +174,14 @@ class Signup extends Component {
     //   console.log('you clicked');
     //   this.props.githubLogin(this.props.history);
     // };
+    //Facebook auth
+  const sendFacebookToken = (data) => {
+    this.props.facebookLogin(data, this.props.history);
+  }
+  
+  const responseFacebook = (response) => {
+    sendFacebookToken(response);
+  }
 
     return (
       <ScrollIntoView>
@@ -362,13 +372,20 @@ class Signup extends Component {
                 <i class="fab fa-twitter pr-3 facbook-logo"></i>
                 Sign up with Twitter
               </Link> */}
-              <Link
-                to=""
+              <FacebookLogin 
+              appId="620560692194763"
+              onFailure={responseFacebook}
+              callback={responseFacebook}
+              render={renderProps => (
+                <Link
+                onClick={renderProps.onClick}
                 className="mt-2 form-control login-btn login-btn-facebook reg-btn "
               >
                 <i class="fab fa-facebook pr-3 facbook-logo"></i>
                 Sign up with Facebook
               </Link>
+              )} />
+              
               {/* <Link
                 to=""
                 className="mt-2 form-control login-btn login-btn-github reg-btn "
@@ -418,4 +435,5 @@ export default connect(mapStateToProps, {
   registerUser,
   googleLogin,
   // githubLogin,
+  facebookLogin
 })(Signup);
